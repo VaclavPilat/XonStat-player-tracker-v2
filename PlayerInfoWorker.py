36463b40075b931d0f7e6e90fdc48ca2a0c366c3
Václav Pilát <pilatvacl@seznam.cz>
2021-09-30 08:36:40 +0200

    Using a generator to print data in labels

--- PlayerInfoWorker.py
+++ PlayerInfoWorker.py
@@ -1,5 +1,6 @@
 from Worker import *
 from ColoredWidgets import *
+import time
 
 
 
@@ -33,35 +34,32 @@ class PlayerInfoWorker(Worker):
         """This method is called before this worker is run
         """
         self.window.status.message("Loading information from player profile")
+    
+
+    def __fillLabels(self):
+        """Yields loaded simple player values
+
+        Yields:
+            Iterator[list]: 0 => table row index, 1 => lambda of an emitted signal
+        """
+        yield [0, lambda: self._showName.emit(self.window.player.loadName())]
+        yield [1, lambda: self._showSince.emit(self.window.player.loadSince())]
+        yield [2, lambda: self._showActive.emit(self.window.player.loadActive())]
         
 
     def run(self):
         """Running the Worker task
         """
         self.window.player.loadProfile()
-        # Filling in player info
-        self.window.table.setRowColor(0, "dark-yellow")
-        self._showName.emit(self.window.player.loadName())
-        if self.window.player.error:
-            self.window.table.setRowColor(0, "dark-red")
-        else:
-            self.window.table.setRowColor(0, None)
-        
-        self.window.table.setRowColor(1, "dark-yellow")
-        self._showSince.emit(self.window.player.loadSince())
-        if self.window.player.error:
-            self.window.table.setRowColor(1, "dark-red")
-        else:
-            self.window.table.setRowColor(1, None)
-
-        self.window.table.setRowColor(2, "dark-yellow")
-        self._showActive.emit(self.window.player.loadActive())
-        self._setActiveColor.emit(self.window.player.getActiveColor())
-        if self.window.player.error:
-            self.window.table.setRowColor(2, "dark-red")
-        else:
-            self.window.table.setRowColor(2, None)
-
+        # Filling in simple player variables
+        for label in self.__fillLabels():
+            label[1]()
+            if label[0] == 2:
+                self._setActiveColor.emit(self.window.player.getActiveColor())
+            if self.window.player.error:
+                self.window.table.setRowColor(label[0], "dark-red")
+            else:
+                self.window.table.setRowColor(label[0], None)
         # Changing status
         if self.window.player.error is not None:
             self.window.status.resultMessage("An error occured: " + self.window.player.error, False)
