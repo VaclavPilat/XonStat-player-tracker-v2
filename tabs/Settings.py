ca6eed3d04cfbd5a6c38a2bd45a2b4461df66a88
VaclavPilat <pilatvacl@seznam.cz>
2022-07-09 15:47:59 +0200

    Created a table for settings

--- tabs/Settings.py
+++ tabs/Settings.py
@@ -20,4 +20,38 @@ class Settings(Tab):
     
 
     def createLayout(self):
-        self.layout.addStretch()
+        """Creating tab layout
+        """
+        # Creating search bar
+        self.searchBar = QtWidgets.QLineEdit(self)
+        self.searchBar.setPlaceholderText("Search by setting name")
+        self.searchBar.textChanged.connect(self.__search)
+        self.layout.addWidget(self.searchBar)
+        # Creating table for tracked players
+        self.table = ColoredTable(self)
+        # Setting columns
+        headers = ["Setting name", "Setting type", "Setting value"]
+        self.table.setColumnCount( len(headers) )
+        self.table.setHorizontalHeaderLabels(headers)
+        # Setting column stretching
+        self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        self.layout.addWidget(self.table)
+    
+
+    def __search(self, text: str):
+        """Hiding and showing rows in the table based on input
+
+        Args:
+            text (str): Serched text
+        """
+        for row in range(self.table.rowCount()):
+            containsText = False
+            for column in range(0, 4):
+                widget = self.table.cellWidget(row, column)
+                if not widget == None and type(widget) == ColoredLabel:
+                    # Checking if this label contins HTML
+                    labelText = parseTextFromHTML(widget.text())
+                    if text.lower() in labelText:
+                        containsText = True
+                        break
+            self.table.setRowHidden(row, not containsText)
