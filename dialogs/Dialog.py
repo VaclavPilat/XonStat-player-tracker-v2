e4f947e5157b14a694d8202b5979b2d2ee5700b0
VaclavPilat <pilatvacl@seznam.cz>
2022-07-10 16:57:32 +0200

    Added common layout settings to Dialog class

--- dialogs/Dialog.py
+++ dialogs/Dialog.py
@@ -1,5 +1,8 @@
 from PyQt5 import QtWidgets
 
+from widgets.ColoredButtons import *
+from widgets.Status import *
+
 
 class Dialog(QtWidgets.QDialog):
     """Class for creating a dialog window
@@ -15,8 +18,34 @@ class Dialog(QtWidgets.QDialog):
         """
         self.id = identifier
         super().__init__(parent)
+        # Adding common layout settings
+        self.accepted.connect(self.dialogAccepted)
+        self.rejected.connect(self.dialogRejected)
         self.setFixedSize(500, 250)
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.setLayout(self.layout)
+        self.layout.addStretch()
+        # Adding custom layout settings
         self.createLayout()
+        # Adding the rest of common layout settings
+        self.buttonBox = QtWidgets.QDialogButtonBox(self)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        acceptButton = SaveButton(self)
+        acceptButton.setDefault(True)
+        self.buttonBox.addButton(acceptButton, QtWidgets.QDialogButtonBox.AcceptRole)
+
+        deleteButton = DeleteButton(self)
+        deleteButton.setAutoDefault(False)
+        self.buttonBox.addButton(deleteButton, QtWidgets.QDialogButtonBox.RejectRole)
+
+        self.layout.addWidget(self.buttonBox)
+        self.layout.addStretch()
+        self.status = Status(self)
+        self.layout.addWidget(self.status)
+        # Showing dialog window
         self.exec()
     
     
@@ -24,3 +53,15 @@ class Dialog(QtWidgets.QDialog):
         """Creating dialog layout
         """
         pass
+
+
+    def dialogAccepted(self):
+        """Called when this dialog is accepted
+        """
+        print("Dialog accepted")
+
+
+    def dialogRejected(self):
+        """Called when this dialog is rejected
+        """
+        print("Dialog rejected")
