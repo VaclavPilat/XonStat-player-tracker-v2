bc179c2eb892fed1fc491920af795ea1460a3be8
VaclavPilat <pilatvacl@seznam.cz>
2022-07-23 10:06:49 +0200

    Added widgets for player table to ServerInfo

--- tabs/ServerInfo.py
+++ tabs/ServerInfo.py
@@ -1,6 +1,9 @@
 from tabs.TabInfo import *
 from widgets.ColoredButtons import *
 from workers.ServerInfoWorker import *
+from dialogs.AddPlayerDialog import *
+from dialogs.DeletePlayerDialog import *
+from dialogs.EditPlayerDialog import *
 
 
 class ServerInfo(TabInfo):
@@ -31,9 +34,31 @@ class ServerInfo(TabInfo):
         browserButton.clicked.connect(lambda: openInBrowser("https://stats.xonotic.org/server/" + str(self.id)))
         self.info.cellWidget(0, 1).layout().addWidget(browserButton)
         # Adding widgets to layout
+        self.scrollLayout.addWidget(self.__createTable())
         self.scrollLayout.addWidget(self.__createGameList())
         # Adding stretch
         self.scrollLayout.addStretch()
+
+    
+    def __createTable(self) -> ColoredFixedTable:
+        """Creates a table for list of players
+
+        Returns:
+            ColoredFixedTable: Table widget
+        """
+        self.players = ColoredFixedTable(self)
+        # Setting columns
+        headers = ["Player ID", "Player name", "Nickname", "Description", "Score", "Actions"]
+        self.players.setColumnCount( len(headers) )
+        self.players.setHorizontalHeaderLabels(headers)
+        # Setting column stretching
+        self.players.verticalHeader().hide()
+        self.players.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        for i in range(1, 4):
+            self.players.horizontalHeader().setSectionResizeMode(i, QtWidgets.QHeaderView.Stretch)
+        for i in range(4, len(headers)):
+            self.players.horizontalHeader().setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeToContents)
+        return self.players
     
 
     def __createGameList(self) -> ColoredFixedTable:
@@ -75,7 +100,75 @@ class ServerInfo(TabInfo):
         """Removes old information to make space for new ones
         """
         super().clearOldInformation()
+        self.players.setRowCount(0)
         self.gameList.setRowCount(0)
+    
+
+    def showPlayer(self, identifier: int, name: str, score: int, color: str):
+        """Adds a row with player information into table
+
+        Args:
+            identifier (int): Player ID
+            name (str): Player name (at the time this game happened)
+            score (int): Player score
+            color (str): Row background color
+        """
+        # Checking if player exists
+        player = checkPlayerExistence(identifier)
+        if player is not None:
+            nickname = player["nick"]
+            description = player["description"]
+        else:
+            nickname = ""
+            description = ""
+            if not color == None:
+                color = "dark-" + color
+        # Creating a new row inside the table
+        row = self.players.rowCount()
+        self.players.insertRow(row)
+        # Adding labels
+        for i in range(5):
+            self.players.setCellWidget(row, i, ColoredLabel(self.players, None, color))
+        # Adding label text
+        self.players.cellWidget(row, 0).setText(str(identifier))
+        self.players.cellWidget(row, 1).setText(name)
+        self.players.cellWidget(row, 2).setText(nickname)
+        self.players.cellWidget(row, 3).setText(description)
+        self.players.cellWidget(row, 4).setText(str(score))
+        # Adding buttons
+        actions = ColoredWidget()
+        buttonGroup = QtWidgets.QHBoxLayout()
+        actions.setLayout(buttonGroup)
+        actions.setBackground(color)
+        buttonGroup.setContentsMargins(0, 0, 0, 0)
+        buttonGroup.setSpacing(0)
+        buttonGroup.addStretch()
+        if identifier >= 6:
+            # Profile button
+            profileButton = BrowserButton(self.players)
+            profileButton.clicked.connect(lambda: webbrowser.open("https://stats.xonotic.org/player/" + str(identifier), new=2))
+            buttonGroup.addWidget(profileButton)
+            # Load button
+            infoButton = WindowButton(self.players)
+            infoButton.clicked.connect(lambda: self.parent.openPlayerInfo(identifier))
+            buttonGroup.addWidget(infoButton)
+            # Adding buttons based on if the player is already bing tracked
+            if player is not None:
+                # Edit button
+                editButton = EditButton(self.players)
+                editButton.clicked.connect(lambda: EditPlayerDialog(self.parent, identifier))
+                buttonGroup.addWidget(editButton)
+                # Delete button
+                deleteButton = DeleteButton(self.players)
+                deleteButton.clicked.connect(lambda: DeletePlayerDialog(self.parent, identifier))
+                buttonGroup.addWidget(deleteButton)
+            else:
+                # Add button
+                addButton = AddButton(self.players)
+                addButton.clicked.connect(lambda: AddPlayerDialog(self.parent, identifier))
+                buttonGroup.addWidget(addButton)
+        buttonGroup.addStretch()
+        self.players.setCellWidget(row, 5, actions)
     
 
     def showRecentGame(self, game: dict):
